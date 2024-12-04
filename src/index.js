dd8c34b286d71815b38339868986e5275f5cfa60
AnastasiiaTL <107275145+AnastasiiaTL@users.noreply.github.com>
2024-12-04 15:12:16 +0100

    Added the AL API integration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
     new Typewriter("#poem", {
-        strings: ["Der du von dem Himmel bist"],
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-      });
-
-    poemElement.innerHTML = "Der du von dem Himmel bist"; 
-}
-
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+  
+  function generatePoem(event) {
+    event.preventDefault();
+  
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+    let context =
+      "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning. Without any marks at the beginning and at the end. 'SheCodes AI' is always on a new line";
+    let prompt = `User instructions: Generate a Germany poem about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a Germany poem about ${instructionsInput.value}</div>`;
+  
+    axios.get(apiURL).then(displayPoem);
+  }
+  
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
